4116a322b085a37652c8b40f94d63752ff5838dc
Saurabh kumar Mishra <71021386+saurabhmishra2001@users.noreply.github.com>
2022-06-21 23:14:25 +0530

    Add files via upload
    In this we check that the given input id Integer or not.

---
+++ intOrNot.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class intOrNot {
+    public static void main(String[]args){
+
+    System.out.print("Enter the Number: ");
+    Scanner num= new Scanner(System.in);
+    System.out.println(num.hasNextInt());
+    }
+}
+
