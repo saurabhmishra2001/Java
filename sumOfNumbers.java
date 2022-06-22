5b07b779049e0e921f3c3786f16826d620c9b63c
Saurabh kumar Mishra <71021386+saurabhmishra2001@users.noreply.github.com>
2022-06-22 10:17:20 +0530

    Add files via upload

---
+++ sumOfNumbers.java
@@ -0,0 +1,9 @@
+public class sumOfNumbers {
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+        int c=30;
+        int sum=a+b+c;
+        System.out.println("The sum is: "+sum);
+    }
+}
