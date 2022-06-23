23e8f34e3126659a67ef59f80ee3fc7765d9ae27
Saurabh kumar Mishra <71021386+saurabhmishra2001@users.noreply.github.com>
2022-06-23 21:43:18 +0530

    Replace spaces with Underscore.
    Java program to replace spaces with underscores.

---
+++ replace.java
@@ -0,0 +1,7 @@
+public class replace {
+    public static void main(String[]args){
+        String text = "To My  Friend";
+        text=text.replace(" ","_");
+        System.out.println(text);
+    }
+}
