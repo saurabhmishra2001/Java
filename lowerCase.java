5f96664f94e57048099ceea977e44c9f4bdca7fb
Saurabh kumar Mishra <71021386+saurabhmishra2001@users.noreply.github.com>
2022-06-23 21:39:08 +0530

    To Lower Case
    Java program to convert a string to lowercase.

---
+++ lowerCase.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+
+public class lowerCase {
+    public static void main(String[]args){
+        String name="SAURABH";
+        name=name.toLowerCase();
+        System.out.println(name);
+    }
+}
